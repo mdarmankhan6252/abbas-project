5b38a58c329454b1ddc425e6d12dfc5351ad0472
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2025-02-16 16:42:06 +0600

    I have added the ebook page

--- src/pages/E-book/EBook.jsx
+++ src/pages/E-book/EBook.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import eBook from '../../assets/eBook.jpg';
+import book from '../../assets/book22.jpeg';
 
 const EBook = () => {
+   const [isModalOpen, setIsModalOpen] = useState(false);
+
+   const openModal = () => {
+      setIsModalOpen(true);
+   };
+
+   const closeModal = () => {
+      setIsModalOpen(false);
+   };
+
    return (
       <div>
          <div className="max-w-5xl mx-auto px-8 py-8 flex justify-center gap-16 pt-20">
@@ -15,7 +27,7 @@ const EBook = () => {
                   </div>
                </div>
                <div className="flex items-center justify-between p-4 gap-6">
-                  <Link to="/e-book" className="bg-gradient-to-r from-primary to-[#1253cd] hover:from-[#1253cd] hover:to-primary duration-300  text-white rounded-lg p-4 grow text-center">ইবুকটি পড়ুন</Link>
+                  <button onClick={openModal} className="bg-gradient-to-r from-primary to-[#1253cd] hover:from-[#1253cd] hover:to-primary duration-300 text-white rounded-lg p-4 grow text-center">ইবুকটি পড়ুন</button>
                   <Link to="/e-book" className="border p-4 rounded-lg hover:bg-black hover:text-white grow text-center">অর্ডার করুন</Link>
                </div>
             </div>
@@ -24,7 +36,7 @@ const EBook = () => {
          {/* about us */}
 
          <div className="bg-gradient-to-r from-primary to-[#1253cd] py-16 text-white text-center px-8 mt-6 ">
-            <h1 className=" flex flex-col items-center justify-center text-lg md:text-3xl font-semibold space-y-4 max-w-4xl mx-auto">  
+            <h1 className="flex flex-col items-center justify-center text-lg md:text-3xl font-semibold space-y-4 max-w-4xl mx-auto">  
                <span>আপনি যদি সত্যিই সফল হতে চান</span>
                <span>তবে এই বইটি আপনার প্রথম পদক্ষেপ হতে পারে</span>
             </h1>
@@ -32,9 +44,16 @@ const EBook = () => {
 
             <p className="font-light text-lg max-w-4xl mx-auto">এই বইটি শুধু একটি বই নয় এটি আপনার জীবনকে পরিবর্তন করার এক চমৎকার উপায় এ বইটি আপনাকে পথ দেখাবে,  পড়ুন আর আপনার জীবন বদলে ফেলুন</p>
 
-            <Link target="_blank" to="https://www.facebook.com/profile.php?id=61572466610055" className="inline-block  bg-black text-white py-3 md:py-4 px-10 mt-6 rounded-lg hover:bg-transparent border-2  hover:border-white text-sm md:text-[16px]">যোগাযোগ করুন</Link>
-            
+            <Link target="_blank" to="https://www.facebook.com/profile.php?id=61572466610055" className="inline-block bg-black text-white py-3 md:py-4 px-10 mt-6 rounded-lg hover:bg-transparent border-2 hover:border-white text-sm md:text-[16px]">যোগাযোগ করুন</Link>
          </div>
+
+         {isModalOpen && (
+            <div className="fixed inset-0 flex items-center justify-center px-8 md:px-2 bg-black/40 bg-opacity-50 transition-opacity duration-300 " onClick={closeModal}>
+               <div className="bg-white rounded-2xl shadow-lg transition-transform duration-300 max-w-2xl mx-auto" onClick={(e) => e.stopPropagation()}>
+                  <img src={book} alt="EBook" className="w-full rounded-2xl" />
+               </div>
+            </div>
+         )}
       </div>
    );
 };
